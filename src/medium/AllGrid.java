c9a32f98fec6e4fc9826b4d173767cdfcb98cf55
Serhii Velmyk <sergiyvelmik@gmail.com>
2019-04-07 11:30:27 +0300

    added method which generates all possible 3x3 grid with digits (0 - 9) where all rows, columns and diagonals have the same sum

--- src/medium/AllGrid.java
+++ src/medium/AllGrid.java
@@ -16,27 +16,15 @@ public class AllGrid {
             array[1][1] = i;
 
             for (int j = 7 * rowSum; j <= 67 * rowSum; j += 9) {
-                int sumFirst = 0;
-                for (int k = 2, value = j; k >= 0 & value > 0; k--) {
-                    if (value % 10 <= rowSum - i) {
-                        array[0][k] = value % 10;
-                        sumFirst += value % 10;
-                        value = value / 10;
-                    } else {
-                        break;
-                    }
+                for (int k = 2, value = j, m = 0; k >= 0; k--, m++, value /= 10) {
+                    array[0][k] = value % 10;
+                    array[2][m] = rowSum - array[1][1] - array[0][k];
                 }
-
-                if (sumFirst != rowSum)
-                    continue;
-
-                array[2][0] = rowSum - array[0][2] - array[1][1];
-                array[2][1] = rowSum - array[0][1] - array[1][1];
-                array[2][2] = rowSum - array[0][0] - array[1][1];
                 array[1][2] = array[1][1] + array[0][0] - array[0][2];
                 array[1][0] = array[1][1] + array[0][2] - array[0][0];
 
-                if (array[1][2] <= 9 && array[1][2] >= 0 && array[1][0] <= 9 && array[1][0] >= 0 && array[2][1] <= 9)
+                if (array[2][0] + array[2][1] + array[2][2] == rowSum && array[1][2] >= 0 && array[1][0] >= 0
+                        && array[2][1] >= 0 && array[1][2] <= 9 && array[1][0] <= 9 && array[2][1] <= 9)
                     System.out.println(Arrays.deepToString(array));
             }
         }
